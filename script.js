87903f15c10245b821c7012fe31088b2a6f33bf8
ShasankLamsal <shashanklamsal4@gmail.com>
2025-05-31 07:31:33 +0545

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("theme-toggle").onclick = function () {
-  document.body.classList.toggle("dark");
-};
-function copyCode(btn) {
-  const code = btn.closest('div').querySelector('code').innerText;
-  navigator.clipboard.writeText(code);
-  btn.textContent = 'Copied!';
-  setTimeout(() => (btn.textContent = 'Copy'), 1500);
-}
